70452b35e379196bc1722e24f82cfc9961a337e8
Stephen <stephen.yuchen.kao@gmail.com>
2015-07-24 20:25:40 -0400

    blah blah blah

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -1,24 +1,33 @@
 /*global define */
 define([
-        'lib/jquery'
+    'lib/jquery'
 ], function (
-        $
+    $
 ) {
 	'use strict';
 
-        jQuery('document').ready(function () {
-                var $baguajing = jQuery('.baguajing');
-
-                $baguajing.on('click', '.trigram',  function () {
-                        var $this = jQuery(this);
-
-                        $baguajing
-                                .removeClass(function (index, className) {
-                                        debugger;
-
-                                        return (className.match(/spun--[1-9]/g) || []).join(' ');
-                                })
-                                .addClass('spun--' + ($this.index() + 1));
-                });
+    jQuery('document').ready(function () {
+        var $baguajing = jQuery('.baguajing');
+        $baguajing.on('click', function () {
+            jQuery('body').toggleClass('daytime nighttime');
         });
+
+        var $clock = jQuery('.js_clock__digits');
+
+        var updateClock = function () {
+            var now = new Date(),
+                hours = now.getHours(),
+                minutes = now.getMinutes(),
+                seconds = now.getSeconds();
+
+            hours = (hours < 10) ? '0' + hours : hours;
+            minutes = (minutes < 10) ? '0' + minutes : minutes;
+            seconds = (seconds < 10) ? '0' + seconds : seconds;
+
+            $clock.text(hours + ':' + minutes + ':' + seconds);
+        };
+
+        updateClock();
+        window.setInterval(updateClock, 100);
+    });
 });

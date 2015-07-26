77bf8382563a0893505d0b1f0a4687b400f4b074
Stephen <stephen.yuchen.kao@gmail.com>
2015-07-26 14:57:14 -0400

    public --> source

--- config.rb
+++
@@ -1,9 +0,0 @@
-require 'modular-scale'
-
-http_path = ""
-css_dir = "target/css"
-sass_dir = "public/scss"
-images_dir = "public/images"
-javascripts_dir = "public/javascripts"
-
-output_style = :compressed

549fd040e8f2798f51955656cd6a0ba4687b538b
Stephen <stephen.yuchen.kao@gmail.com>
2015-07-28 21:54:22 -0400

    Local Webpack installation...you dumbass

--- gulpfile.js
+++ gulpfile.js
@@ -4,10 +4,10 @@
 
 var gulp = require('gulp'),
     gulpUtil = require('gulp-util'),
+    webpack = require('webpack'),
     plugins = require('gulp-load-plugins')({
         lazy: false
   });
-
 
 ////////// Development tasks
 gulp.task('css:dev', function () {
@@ -41,6 +41,7 @@ gulp.task('css:dev', function () {
 
 gulp.task('javascript:dev', function () {
     gulp.start('jshint:dev');
+    gulp.start('webpack:dev');
 });
 
 gulp.task('jshint:dev', function() {
@@ -63,6 +64,30 @@ gulp.task('jshint:dev', function() {
         .pipe(plugins.jshint.reporter('jshint-stylish'));
 });
 
+gulp.task('webpack:dev', function (callback) {
+    var myConfig = Object.create(require('./webpack.config.js'));
+
+    /*
+    myConfig.plugins = myConfig.plugins.concat(
+        new webpack.DefinePlugin({
+            'process.env': {
+                // This has effect on the react lib size
+                'NODE_ENV': JSON.stringify('production')
+            }
+        }),
+        new webpack.optimize.DedupePlugin(),
+        new webpack.optimize.UglifyJsPlugin()
+    );
+*/
+
+    webpack(myConfig, function (err, stats) {
+        if (err) throw new gulpUtil.PluginError("webpack:build-dev", err);
+        gulpUtil.log("[webpack:build-dev]", stats.toString({
+            colors: true
+        }));
+    });
+});
+
 ////////// Deployment tasks
 gulp.task('css:deploy', function () {
     gulp.src('source/scss/**/*.scss')
@@ -75,9 +100,32 @@ gulp.task('css:deploy', function () {
 
 gulp.task('javascript:deploy', function () {});
 
+gulp.task('webpack:deploy', function () {
+    var myConfig = Object.create(require('./webpack.config.js'));
+
+    myConfig.plugins = myConfig.plugins.concat(
+        new webpack.DefinePlugin({
+            'process.env': {
+                // This has effect on the react lib size
+                'NODE_ENV': JSON.stringify('production')
+            }
+        }),
+        new webpack.optimize.DedupePlugin(),
+        new webpack.optimize.UglifyJsPlugin()
+    );
+
+    webpack(myConfig, function (err, stats) {
+        if (err) throw new gulpUtil.PluginError("webpack:build-dev", err);
+        gulpUtil.log("[webpack:build-dev]", stats.toString({
+            colors: true
+        }));
+    });
+});
+
 gulp.task('dev', function () {
     gulp.watch(['source/javascript/**/*.js'], [
-        'javascript:dev'
+        'javascript:dev',
+        'webpack:dev'
     ]);
 
     // Initial SCSS compilation
@@ -89,4 +137,5 @@ gulp.task('dev', function () {
 
 gulp.task('deploy', function () {
     gulp.start('css:deploy');
+    gulp.start('javascript:deploy');
 });

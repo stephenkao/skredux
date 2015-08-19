ed6aabf2d0cd338db6a7a6fe434e614eb5a6bacb
Stephen Kao <stephen.kao@gawker.com>
2015-08-19 17:59:43 -0400

    Routing

--- source/javascript/page/blog.js
+++ source/javascript/page/blog.js
@@ -1,5 +1,36 @@
 /*global module */
 
 import React from 'react';
+import { RouteHandler, Link} from 'react-router';
 
-var BlogLayout, PostView;
+var BlogLayout, BlogPost;
+
+BlogLayout = React.createClass({
+    render: function () {
+        return (
+            <ul>
+                <BlogPost />
+                <BlogPost />
+                <BlogPost />
+                <BlogPost />
+                <BlogPost />
+                <BlogPost />
+                <BlogPost />
+                <BlogPost />
+                <BlogPost />
+            </ul>
+        );
+    }
+});
+
+BlogPost = React.createClass({
+    render: function () {
+        return (
+            <div>
+                <p style={{color: 'black'}}>POST POST POST</p>
+            </div>
+        );
+    }
+});
+
+module.exports = BlogLayout;
--- source/javascript/router.js
+++ source/javascript/router.js
@@ -4,14 +4,14 @@ import React from 'react';
 import { Router, Route, Link, DefaultRoute } from 'react-router';
 
 var NavigationLayout = require('./page/home');
-var BlogLayout = require('./page/home');
+var BlogLayout = require('./page/blog');
 
 var routes = (
-    <Route name='app' path='/' handler={NavigationLayout}>
-        <Route name='sanguine' path='/' handler={NavigationLayout} />
-        <Route name='melancholic' path='/' handler={NavigationLayout} />
-        <Route name='phlegmatic' path='/' handler={NavigationLayout} />
-        <Route name='choleric' path='/' handler={NavigationLayout} />
+    <Route path='/' handler={NavigationLayout}>
+        <Route name='sanguine' path='/life' handler={NavigationLayout} />
+        <Route name='melancholic' path='/script' handler={BlogLayout} />
+        <Route name='phlegmatic' path='/mirth' handler={NavigationLayout} />
+        <Route name='choleric' path='/toil' handler={NavigationLayout} />
     </Route>
 );
 

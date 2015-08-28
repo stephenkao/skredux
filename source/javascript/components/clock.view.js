053d22888500c2ed06bb44e353f4b7cc6b4be8b3
Stephen Kao <stephen.yuchen.kao@gmail.com>
2015-08-27 21:22:18 -0400

    Nested Components

---
+++ source/javascript/components/clock.view.js
@@ -0,0 +1,75 @@
+/*global window, require, module */
+
+'use strict';
+
+////////// Dependencies
+var React = require('react');
+
+////////// Entry point
+
+var ClockView = React.createClass({
+    /**
+     * On initializing this View, pass in the current time
+     *
+     * @return {!{hours: number, minutes: number, seconds: number}}
+     */
+    getInitialState: function () {
+        return this.getTime();
+    },
+
+    render: function () {
+        return (
+            <div className="menu">
+                <div className="menu__trigger framed framed--circle"></div>
+                <div className="menu__title">
+                    <span>[Antechamber]</span>
+                    <time>
+                        {this.state.hours < 10 ? '0' + this.state.hours : this.state.hours}:
+                        {this.state.minutes < 10 ? '0' + this.state.minutes : this.state.minutes}:
+                        {this.state.seconds < 10 ? '0' + this.state.seconds : this.state.seconds}
+                    </time>
+                </div>
+            </div>
+        );
+    },
+
+    /**
+     * On mounting this View, kick off the ticking clock
+     *
+     * VOID->VOID
+     */
+    componentDidMount: function () {
+        window.setInterval(this.updateTime, 1000);
+    },
+
+    ////////// Helpers
+    /**
+     * A convenience function that just returns a parsed time object
+     * @return {!{hours: number, minutes: number, seconds: number}}
+     */
+    getTime: function () {
+        var date = new Date();
+
+        return {
+            hours: date.getHours(),
+            minutes: date.getMinutes(),
+            seconds: date.getSeconds()
+        };
+    },
+    /**
+     * A method that updates the time state of this View
+     *
+     * VOID->VOID
+     */
+    updateTime: function () {
+        var time = this.getTime();
+
+        this.setState({
+            hours: time.hours,
+            minutes: time.minutes,
+            seconds: time.seconds
+        });
+    }
+});
+
+module.exports = ClockView;
--- source/javascript/index.js
+++ source/javascript/index.js
@@ -2,62 +2,32 @@
 
 'use strict';
 
+////////// Dependencies
 var React = require('react'),
     Router = require('react-router'),
     Route = Router.Route,
     RouteHandler = Router.RouteHandler,
     DefaultRoute = Router.DefaultRoute;
 
-var HomeLayout = require('./page/home'),
-    BlogLayout = require('./page/blog');
+var HomeLayout = require('./pages/home.layout'),
+    ClockView = require('./components/clock.view');
 
-////////// Main app
+////////// Entry point
 
-var App = React.createClass({
-    getInitialState: function () {
-        var date = new Date();
-        var hour = date.getHours();
-        var minute = date.getMinutes();
-        var second = date.getSeconds();
-
-        return {
-            hour: hour,
-            minute: minute,
-            second: second
-        };
-    },
-
-    clockTick: function(){
-        var date = new Date();
-        var hour = date.getHours();
-        var minute = date.getMinutes();
-        var second = date.getSeconds();
-        this.setState({hour: hour, minute: minute, second: second});
-    },
-
+var AppLayout = React.createClass({
     render: function () {
         return (
-            <div className="main">
-                <div className="menu">
-                    <div className="menu__trigger framed framed--circle"></div>
-                    <div className="menu__title">
-                        <span>[Antechamber]</span>
-                        <time>{this.state.hour}:{this.state.minute}:{this.state.second}</time>
-                    </div>
-                </div>
+            <div>
+                <ClockView />
 
                 <RouteHandler/>
             </div>
         );
-    },
-
-    componentDidMount: function () {
-        window.setInterval(this.clockTick, 1000);
     }
 });
 
 var routes = (
-    <Route handler={App}>
+    <Route handler={AppLayout}>
         <Route name="biography" path="biography" handler={HomeLayout} />
         <Route name="experience" path="experience" handler={HomeLayout} />
         <Route name="recreation" path="recreation" handler={HomeLayout} />
